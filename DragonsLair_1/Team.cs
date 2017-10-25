9bdc1124e2e772ebb89a58dc02e492b0756b7ef4
kristoffer petersen <krizzmp@gmail.com>
2017-10-25 10:27:13 +0200

    adds the provided code skeleton

---
+++ DragonsLair_1/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DragonsLair_1
+{
+    public class Team
+    {
+        public string Name { get; set; }
+
+        public Team(string teamName)
+        {
+            Name = teamName;
+        }
+    }
+}
