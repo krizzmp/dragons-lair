b9624be9652a9c1989666af96e3ae3af27dc33da
kristoffer petersen <krizzmp@gmail.com>
2017-10-25 11:57:47 +0200

    implements GetMatch

--- DragonsLair_1/Round.cs
+++ DragonsLair_1/Round.cs
@@ -1,34 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
-namespace DragonsLair_1
-{
-    public class Round
-    {
+namespace DragonsLair_1 {
+    public class Round {
         private List<Match> matches = new List<Match>();
-        
-        public void AddMatch(Match m)
-        {
+
+        public void AddMatch(Match m) {
             matches.Add(m);
         }
 
-        public Match GetMatch(string teamName1, string teamName2)
-        {
-            // TODO: Implement this method
-            return null;
+        public Match GetMatch(string teamName1, string teamName2) {
+            return matches.Where(m => (
+                m.FirstOpponent.Name == teamName1 ||
+                m.FirstOpponent.Name == teamName2 ||
+                m.SecondOpponent.Name == teamName1 ||
+                m.SecondOpponent.Name == teamName2
+            )).Single();
         }
 
         public bool IsMatchesFinished() {
             return matches.TrueForAll(m => m.Winner != null);
         }
 
-        public List<Team> GetWinningTeams()
-        {
+        public List<Team> GetWinningTeams() {
             // TODO: Implement this method
             return null;
         }
 
-        public List<Team> GetLosingTeams()
-        {
+        public List<Team> GetLosingTeams() {
             // TODO: Implement this method
             return null;
         }
