8d837121eb322f2368bcc7c25f8eb3933084ce7f
kristoffer petersen <krizzmp@gmail.com>
2017-10-27 10:11:46 +0200

    implements Tournament.AddRound

--- DragonsLair_1/Tournament.cs
+++ DragonsLair_1/Tournament.cs
@@ -5,10 +5,56 @@ namespace DragonsLair_1
     public class Tournament
     {
         public string Name { get; set; }
+        private List<Round> rounds = new List<Round>();
 
         public Tournament(string tournamentName)
         {
             Name = tournamentName;
+            //Round r1 = new Round();
+            //Match match11 = new Match();
+            //match11.FirstOpponent = new Team("The Valyrians");
+            //match11.SecondOpponent = new Team("The Spartans");
+            //match11.Winner = new Team("The Valyrians");
+            //r1.AddMatch(match11);
+
+            //Match match12 = new Match();
+            //match12.FirstOpponent = new Team("The Cretans");
+            //match12.SecondOpponent = new Team("The Thereans");
+            //match12.Winner = new Team("The Thereans");
+            //r1.AddMatch(match12);
+
+            //Match match13 = new Match();
+            //match13.FirstOpponent = new Team("The Coans");
+            //match13.SecondOpponent = new Team("The Cnideans");
+            //match13.Winner = new Team("The Coans");
+            //r1.AddMatch(match13);
+
+            //Match match14 = new Match();
+            //match14.FirstOpponent = new Team("The Megareans");
+            //match14.SecondOpponent = new Team("The Corinthians");
+            //match14.Winner = new Team("The Corinthians");
+            //r1.AddMatch(match14);
+            //rounds.Add(r1);
+            //Round r2 = new Round();
+
+            //Match match21 = new Match();
+            //match21.FirstOpponent = new Team("The Valyrians");
+            //match21.SecondOpponent = new Team("The Thereans");
+            //match21.Winner = new Team("The Valyrians");
+            //r2.AddMatch(match21);
+
+            //Match match22 = new Match();
+            //match22.FirstOpponent = new Team("The Coans");
+            //match22.SecondOpponent = new Team("The Corinthians");
+            //match22.Winner = new Team("The Coans");
+            //r2.AddMatch(match22);
+
+            //Round r3 = new Round();
+            //Match match31 = new Match();
+            //match31.FirstOpponent = new Team("The Valyrians");
+            //match31.SecondOpponent = new Team("The Coans");
+            //match31.Winner = new Team("The Coans");
+            //r3.AddMatch(match31);
         }
 
         public List<Team> GetTeams()
@@ -26,63 +72,19 @@ namespace DragonsLair_1
             });
         }
 
+        public void AddRound(Round round)
+        {
+            rounds.Add(round);
+        }
+
         public int GetNumberOfRounds()
         {
-            return 3;
+            return rounds.Count;
         }
 
         public Round GetRound(int idx)
         {
-            Round r = new Round();
-            if (idx == 0)
-            {
-                Match match1 = new Match();
-                match1.FirstOpponent = new Team("The Valyrians");
-                match1.SecondOpponent = new Team("The Spartans");
-                match1.Winner = new Team("The Valyrians");
-                r.AddMatch(match1);
-
-                Match match2 = new Match();
-                match2.FirstOpponent = new Team("The Cretans");
-                match2.SecondOpponent = new Team("The Thereans");
-                match2.Winner = new Team("The Thereans");
-                r.AddMatch(match2);
-
-                Match match3 = new Match();
-                match3.FirstOpponent = new Team("The Coans");
-                match3.SecondOpponent = new Team("The Cnideans");
-                match3.Winner = new Team("The Coans");
-                r.AddMatch(match3);
-
-                Match match4 = new Match();
-                match4.FirstOpponent = new Team("The Megareans");
-                match4.SecondOpponent = new Team("The Corinthians");
-                match4.Winner = new Team("The Corinthians");
-                r.AddMatch(match4);
-            }
-            else if (idx == 1)
-            {
-                Match match1 = new Match();
-                match1.FirstOpponent = new Team("The Valyrians");
-                match1.SecondOpponent = new Team("The Thereans");
-                match1.Winner = new Team("The Valyrians");
-                r.AddMatch(match1);
-
-                Match match2 = new Match();
-                match2.FirstOpponent = new Team("The Coans");
-                match2.SecondOpponent = new Team("The Corinthians");
-                match2.Winner = new Team("The Coans");
-                r.AddMatch(match2);
-            }
-            else if (idx == 2)
-            {
-                Match match1 = new Match();
-                match1.FirstOpponent = new Team("The Valyrians");
-                match1.SecondOpponent = new Team("The Coans");
-                match1.Winner = new Team("The Coans");
-                r.AddMatch(match1);
-            }
-            return r;
+            return rounds[idx];
         }
     }
 }
